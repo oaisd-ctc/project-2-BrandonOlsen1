5f8fe60b00870c9a38ec894ec50897057b6174df
Brandon Olsen <112421088+BrandonOlsen1@users.noreply.github.com>
2024-03-12 17:59:55 +0000

    form work

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,57 +1,56 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const addTask = (task) => {
-    setTasks([...tasks, task]);
-  };
-
-  return (
-    <>
-      <div className="to-do-form">
-        <div className="header">
-          <h1>To do's</h1>
-        </div>
-        <MyForm addTask={addTask} />
-        <ListOfTasks tasks={tasks} />
-      </div>
-    </>
-  );
-}
-
-function MyForm({ addTask }) {
-  const [task, setTask] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    addTask(task);
-    setTask("");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        id="inputed-to-dos"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-      />
-      <input type="submit" value="Submit" />
-    </form>
-  );
-}
-
-function ListOfTasks({ tasks }) {
-  return (
-    <>
-      <div id="list-of-tasks">
-          {tasks.map((task, index) => (
-            <p key={index}>{task}</p>
-          ))}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="to-do-form">
+                <div className="header">
+                    <h1> To do's </h1>
+                </div>
+                <MyForm />
+                <ListOfTasks />
+            </div>
+        </>
+    );
 }
 
 export default App;
+
+function MyForm() {
+    const [task, setTask] = useState("");
+    const [myTasks, setMyTasks] = useState([]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setMyTasks([...myTasks, task]);
+        console.log(myTasks);
+        setTask("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                id="inputed-to-dos"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+            />
+            <input type="submit" value="Submit" />
+        </form>
+    );
+}
+
+function ListOfTasks() {
+
+
+    return (
+        <>
+            <div id="list-of-tasks">
+                
+            </div>
+        </>
+    );
+}
+
+
