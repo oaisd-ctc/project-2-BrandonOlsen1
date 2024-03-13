2c123c8ee6290ea53e7685efd561b115e6b3afdc
Brandon Olsen <112421088+BrandonOlsen1@users.noreply.github.com>
2024-03-13 17:54:29 +0000

    Complete/delete buttons

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -2,35 +2,37 @@ import React, { useState } from 'react';
 import "./App.css";
 
 function App() {
+    const [myTasks, setMyTasks] = useState([]);
+
+    // Function to add a task
+    const addTask = (task) => {
+        setMyTasks([...myTasks, task]);
+    };
+
     return (
-        <>
-            <div className="to-do-form">
-                <div className="header">
-                    <h1> To do's </h1>
-                </div>
-                <MyForm />
-                <ListOfTasks />
+        <div className="to-do-form">
+            <div className="header">
+                <h1> To do's </h1>
             </div>
-        </>
+            <MyForm addTask={addTask} />
+            <ListOfTasks tasks={myTasks} />
+        </div>
     );
 }
 
-export default App;
-
-function MyForm() {
+function MyForm({ addTask }) {
     const [task, setTask] = useState("");
-    const [myTasks, setMyTasks] = useState([]);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        setMyTasks([...myTasks, task]);
-        console.log(myTasks);
+        addTask(task);
         setTask("");
     };
 
     return (
         <form onSubmit={handleSubmit}>
             <input
+            
                 type="text"
                 id="inputed-to-dos"
                 value={task}
@@ -41,16 +43,19 @@ function MyForm() {
     );
 }
 
-function ListOfTasks() {
-
-
+function ListOfTasks({ tasks }) {
     return (
-        <>
-            <div id="list-of-tasks">
-                
-            </div>
-        </>
+        <div id="list-of-tasks">
+            {tasks.map((task, index) => (
+                <div id="to-do-item">
+                    <p key={index}>{task}</p> 
+                    <button key={index} type='submit'  id="button"  >Complete     </button>
+                    
+                    <button key={index} type='submit' id="button" >     Delete</button>              
+                </div>
+            ))}
+        </div>
     );
 }
 
-
+export default App;
